8992bc6f14d8ed510e93f879bbc07c3c69186fec
Mohamed Elshater <107942995+Elshater6498@users.noreply.github.com>
2024-12-23 21:12:55 +0200

    update pricing part in landing page

--- src/components/landing/Pricing.jsx
+++ src/components/landing/Pricing.jsx
@@ -2,49 +2,60 @@ import { useTranslation } from 'react-i18next'
 import { FiCheckCircle } from 'react-icons/fi'
 const Pricing = () => {
 const {t}=useTranslation("landing")
+
+
+const sharedFeatures = [
+  t("pricing.features.dinamicMenu"),
+  t("pricing.features.qr"),
+  t("pricing.features.logo"),
+  t("pricing.features.appearance"),
+  t("pricing.features.dashboard"),
+  t("pricing.features.socialMedia"),
+  t("pricing.features.googleMap"),
+  t("pricing.features.hours"),
+  t("pricing.features.language"),
+  t("pricing.features.intro"),
+  t("pricing.features.products"),
+  t("pricing.features.categories"),
+  t("pricing.features.customerReviews"),
+  t("pricing.features.whatsapp"),
+  t("pricing.features.edits"),
+  t("pricing.features.team"),
+  t("pricing.features.manager"),
+  t("pricing.features.support"),
+]
+
   const plans = [
     {
       id: 1,
-      title: t("pricing.plan1_title"),
+      planType:t("pricing.basic"),
+      title: t("pricing.basicTitle"),
+      subTitle:t("pricing.basicSubtitle"),
       best_seller: false,
-      month_price: 100,
-      half_year_price: 400,
-      features: [
-        t("pricing.features.feature_1"),
-        t("pricing.features.feature_2"),
-        t("pricing.features.feature_3"),
-        t("pricing.features.feature_4"),
-        t("pricing.features.feature_5"),
-      ],
+      month_price: 150,
+      half_year_price: 600,
+      features: sharedFeatures
     },
     {
       id: 2,
-      title: t("pricing.plan2_title"),
+      planType:t("pricing.traditional"),
+      title: t("pricing.traditionalTitle"),
+      subTitle:t("pricing.traditionalSubtitle"),
       best_seller: true,
-      month_price: 150,
-      half_year_price: 600,
+      month_price: 100,
+      half_year_price: 400,
       features: [
-        t("pricing.features.feature_1"),
-        t("pricing.features.feature_2"),
-        t("pricing.features.feature_3"),
-        t("pricing.features.feature_4"),
-        t("pricing.features.feature_5"),
-      ],
-    },
-    {
-      id: 3,
-      title: t("pricing.plan3_title"),
-      best_seller: false,
-      month_price: 200,
-      half_year_price: 800,
-      features: [
-        t("pricing.features.feature_1"),
-        t("pricing.features.feature_2"),
-        t("pricing.features.feature_3"),
-        t("pricing.features.feature_4"),
-        t("pricing.features.feature_5"),
-      ],
-    },
+        ...sharedFeatures,
+        t("pricing.features.order"),
+        t("pricing.features.type"),
+        t("pricing.features.location"),
+        t("pricing.features.table"),
+        t("pricing.features.tableNumber"),
+        t("pricing.features.notes"),
+        t("pricing.features.print"),
+        t("pricing.features.tracking"),
+      ]
+    }
   ]
 
   return (
@@ -61,11 +72,11 @@ const {t}=useTranslation("landing")
           {t("pricing.subtitle")}
         </p>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-20 py-10'>
         {plans.map((plan) => (
           <div
             key={plan.id}
-            className={`shadow-[0px_5px_20px_0px_rgba(0,0,0,0.2)] flex flex-col rounded-lg relative justify-center pt-10 px-4 ${
+            className={`shadow-[0px_5px_20px_0px_rgba(0,0,0,0.2)] flex flex-col rounded-lg relative h-fit pt-10 px-4 ${
               plan.best_seller ? 'bg-white' : 'bg-slate-900 text-white'
             }`}
           >
@@ -81,8 +92,20 @@ const {t}=useTranslation("landing")
               </>
             ) : null}
             <h4 className='text-center py-4 text-3xl font-bold'>
-              {plan.title}
+              {plan.planType}
             </h4>
+            <p className="text-center text-base sm:text-lg mb-2">{plan.title}</p>
+            <p className="text-center text-base sm:text-lg">{plan.subTitle}</p>
+            {/* <p className='text-center text-3xl font-bold'>
+              {plan.month_price}{' '}
+              <span className='text-base'>{t("pricing.pound")}</span> /{' '}
+              <span className='text-lg font-normal'>{t("pricing.month")}</span>
+            </p>
+            <p className='text-center text-3xl font-bold'>
+              {plan.half_year_price}{' '}
+              <span className='text-base'>{t("pricing.pound")}</span> /{' '}
+              <span className='text-lg font-normal'>6 {t("pricing.months")}</span>
+            </p> */}
             <div className='py-6'>
               {plan.features.map((item, i) => (
                 <div key={i} className='flex items-center gap-2 px-2 py-3'>
@@ -97,21 +120,11 @@ const {t}=useTranslation("landing")
                 </div>
               ))}
             </div>
-            <p className='text-center text-3xl font-bold'>
-              {plan.month_price}{' '}
-              <span className='text-base'>{t("pricing.pound")}</span> /{' '}
-              <span className='text-lg font-normal'>{t("pricing.month")}</span>
-            </p>
-            <p className='text-center text-3xl font-bold'>
-              {plan.half_year_price}{' '}
-              <span className='text-base'>{t("pricing.pound")}</span> /{' '}
-              <span className='text-lg font-normal'>6 {t("pricing.months")}</span>
-            </p>
             <a
               href='https://api.whatsapp.com/send?phone=+201100124479&text=Hello,%20more%20information!'
               target='_blank'
               rel='noreferrer'
-              className={`py-2 px-8 rounded-full text-center items-center font-semibold tracking-wider text-lg my-6 ${
+              className={`py-2 px-8 rounded-full text-center mx-auto items-center font-semibold tracking-wider text-lg my-6 ${
                 plan.best_seller
                   ? 'bg-slate-900 text-white'
                   : 'bg-white text-slate-900'
